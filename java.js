47806f5830e15cbcdf50e0c71784eadc11f4584e
Rodrigonovato10 <luisrodrigo.furtado123@gmail.com>
2024-01-31 17:13:10 -0300

    Conversor

---
+++ java.js
@@ -0,0 +1,10 @@
+
+const input = document.querySelector("#main-input") 
+const select = document.querySelector("select")
+const button = document.querySelector(".main-button")
+   
+
+function troqueiValor(){
+console.log(troqueiValor)
+}
+button .addEventListener("click", troqueiValor)
