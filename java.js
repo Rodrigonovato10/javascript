4aa986ec56b1bc3a7ec5b1522ec8c873f96def11
Rodrigonovato10 <luisrodrigo.furtado123@gmail.com>
2024-02-12 22:02:11 -0300

    Duas condições  para verdadeiros no "IF"!

--- java.js
+++ java.js
@@ -1,24 +1,37 @@
 const convertbutton = document.querySelector(".bottom")
-const selecPrimaria = document.querySelector(".selecaoend")// Selecionar as moedas para converte
+const selecPrimer = document.querySelector(".Selecaotop") // 1º Opção para selecinar a moeda  corversora: "Selecaotop"!
+const selecSegund = document.querySelector(".selecaoend")// 2º Opção  para selecionar a moeda convertida: "Selecaoend"!
+
 function convertValues(){
 
     const inputCurrency = document.querySelector(".input-coin").value  // valor digitado 
     const currencyToValueConvert = document.querySelector(".currency-to-Convert") // valor para converter
     const currencyValuecorvetd = document.querySelector(".currency-Convertd") //valor convertido
     
-    console.log(selecPrimaria.value)
+console.log(selecSegund.value)
     const dolarToday = 4.97
     const euroToday = 5.36
-    
-    
 
-    if(selecPrimaria.value == "dolar"){
-        currencyValuecorvetd.innerHTML = new Intl.NumberFormat("en-US",{
+    // 1º Opção para selecinar a moeda  corversora: "Selecaotop"!
+
+
+
+
+    // 2º Opção  para selecionar a moeda convertida: "Selecaoend"!
+  
+    
+   if(selecPrimer == "real"){ 
+    console.log(selecPrimer) 
+   } if (selecSegund.value === "dolar"){
+       currencyValuecorvetd.innerHTML = new Intl.NumberFormat("en-US",{
             style: "currency",
             currency: "USD",
-        }).format(inputCurrency/dolarToday)
+        }).format( inputCurrency/dolarToday)
     }
-   if(selecPrimaria.value == "euro"){
+   
+   
+   
+    if(selecSegund.value == "euro"){
     currencyValuecorvetd.innerHTML = new Intl.NumberFormat("de-DE",{
         style: "currency",
         currency: "EUR",
